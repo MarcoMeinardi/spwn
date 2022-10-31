a1c8aaa7a1544d6c2ca7bf35be70cd5bcf9257a2
MarcoMeinardi <marcomeinardi01@gmail.com>
2022-11-01 00:48:46 +0100

    Cleaner setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 import setuptools
+from setuptools.command.install import install
+import os
+
+# Copy configurations in ~/.config/spwn
+class CreateConfigs(install):
+	def run(self, *args, **kwargs):
+		super().run(*args, **kwargs)
+		config_dir = os.path.expanduser("~/.config/spwn")
+		if not os.path.exists(config_dir):
+			os.mkdir(config_dir)
+
+		config_file = os.path.expanduser("~/.config/spwn/config.json")
+		template_file = os.path.expanduser("~/.config/spwn/template.py")
+		if not os.path.exists(config_file):
+			default_config_path = os.path.join(os.path.dirname(__file__), "default-config.json")
+			with open(default_config_path) as f:
+				config = f.read()
+			with open(config_file, "w") as f:
+				f.write(config)
+		if not os.path.exists(template_file):
+			default_template_path = os.path.join(os.path.dirname(__file__), "default-template.py")
+			with open(default_template_path) as f:
+				template = f.read()
+			with open(template_file, "w") as f:
+				f.write(template)
+		
+		return True
 
 with open("README.md") as f:
 	long_description = f.read()
@@ -16,30 +43,10 @@ setuptools.setup(
 	entry_points={
 		"console_scripts": ["spwn = spwn.spwn:main"]
 	},
+	cmdclass={
+		"install": CreateConfigs
+	},
+	
 	url="https://github.com/MarcoMeinardi/spwn"
 )
 
-import os
-# Copy configurations in ~/.config/spwn
-DEFAULT_CONFIGURATIONS = '''
-{
-	"debug_dir": "debug",
-	"script_file": "a.py",
-	"template_file": "~/.config/spwn/template.py"
-}
-'''[1:]
-config_dir = os.path.expanduser("~/.config/spwn")
-if not os.path.exists(config_dir):
-	os.mkdir(config_dir)
-
-config_file = os.path.expanduser("~/.config/spwn/config.json")
-template_file = os.path.expanduser("~/.config/spwn/template.py")
-if not os.path.exists(config_file):
-	with open(config_file, "w") as f:
-		f.write(DEFAULT_CONFIGURATIONS)
-if not os.path.exists(template_file):
-	default_template_path = os.path.join(os.path.dirname(__file__), "spwn/default-template.py")
-	with open(default_template_path) as f:
-		template = f.read()
-	with open(template_file, "w") as f:
-		f.write(template)
