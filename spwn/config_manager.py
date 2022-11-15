ecc901a84ad6666faff9b80131d037f70952e4da
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2022-11-15 23:06:44 +0100

    Replace spaces with tabs
    Why, just why?

--- spwn/config_manager.py
+++ spwn/config_manager.py
@@ -2,39 +2,39 @@ import json
 import os
 
 class ConfigManager:
-    def __init__(self, config_path) -> None:
-        try:
-            self.config_path = os.path.expanduser(config_path)
-            self.configs = json.load(open(self.config_path))
-        except FileNotFoundError:
-            self.configs = {}
+	def __init__(self, config_path) -> None:
+		try:
+			self.config_path = os.path.expanduser(config_path)
+			self.configs = json.load(open(self.config_path))
+		except FileNotFoundError:
+			self.configs = {}
 
-        self.default_configs = json.load(open(os.path.expanduser("~/.config/spwn/.default_config.json")))
+		self.default_configs = json.load(open(os.path.expanduser("~/.config/spwn/.default_config.json")))
 
-    def __getitem__(self, key):
-        if key in self.configs:
-            value = self.configs[key]
-        elif key in self.default_configs:
-            value = self.default_configs[key]
-        else:
-            value = None
+	def __getitem__(self, key):
+		if key in self.configs:
+			value = self.configs[key]
+		elif key in self.default_configs:
+			value = self.default_configs[key]
+		else:
+			value = None
 
-        if value and "~" in value:
-            value = os.path.expanduser(value)
+		if value and "~" in value:
+			value = os.path.expanduser(value)
 
-        return value
+		return value
 
-    def __setitem__(self, key, value):
-        self.configs[key] = value
-        json.dump(self.configs, open(self.config_path, "w"), indent='\t')
+	def __setitem__(self, key, value):
+		self.configs[key] = value
+		json.dump(self.configs, open(self.config_path, "w"), indent='\t')
 
-    def keys(self):
-        return self.configs.keys()
+	def keys(self):
+		return self.configs.keys()
 
-    def reset(self, key):
-        if key in self.default_configs:
-            self.configs[key] = self.default_configs[key]
-        else:
-            del self.configs[key]
+	def reset(self, key):
+		if key in self.default_configs:
+			self.configs[key] = self.default_configs[key]
+		else:
+			del self.configs[key]
 
-        json.dump(self.configs, open(self.config_path, "w"), indent='\t')
+		json.dump(self.configs, open(self.config_path, "w"), indent='\t')
