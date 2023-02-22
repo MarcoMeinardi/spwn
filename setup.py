7e6a5f2ef4b76e242245bf98ef067ff1fc662783
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-02-22 20:40:46 +0100

    v1.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setuptools.setup(
 	name="spwn",
-	version="1.2.1",
+	version="1.2.2",
 	author="Chino",
 	description="Automatic tool to quickly start a pwn CTF challenge",
 	long_description=long_description,
